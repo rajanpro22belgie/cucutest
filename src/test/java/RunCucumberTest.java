a41f646e43f6ba98f75073483207f1fa2aed59db
Rangarajan Ilango <rajanpro.22@gmail.com>
2026-03-03 12:10:42 +0100

    Extent reporting addition 2

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -1,4 +1,6 @@
+
 import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
@@ -6,8 +8,9 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
+@IncludeEngines("cucumber")
 @SelectClasspathResource("Features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "stepDefinition")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "stepDefinition,hooks")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class RunCucumberTest {
 }
